2547c832c5497cf9187ac454658e46993c665cff
Mary <cameron105@mail.ru>
2024-02-05 10:32:14 +0300

    unit 19

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -3,20 +3,17 @@ from jsonschema import validate
 from resources.schemas import create_post_user_schema, list_user_schema, update_user_schema, get_user_schema
 
 
-# 1. на каждый из методов GET/POST/PUT/DELETE ручек reqres.in
-# 3. На разные статус-коды 200/201/204/404/400
-
 def test_get_user_check_status_code():
     response = requests.get('https://reqres.in/api/users/2')
     assert response.status_code == 200
 
 
-def test_post_user_check_status_code():
+def test_post_create_user_check_status_code():
     response = requests.post('https://reqres.in/api/users', data={"name":"morpheus","job":"leader"})
     assert response.status_code == 201
 
 
-def test_put_user_check_status_code():
+def test_put_change_user_check_status_code():
     response = requests.put('https://reqres.in/api/users/2', data={"name":"morpheus","job":"zion resident"})
     assert response.status_code == 200
 
@@ -43,9 +40,6 @@ def test_get_resource_not_found():
     assert response.status_code == 404
 
 
-# 2. Позитивные/Негативные тесты на одну из ручек.
-# 5. С ответом
-
 def test_login_successful():
     response = requests.post('https://reqres.in/api/login', data={"email":"eve.holt@reqres.in","password":"cityslicka"})
     assert response.status_code == 200
@@ -60,7 +54,6 @@ def test_login_without_password():
     assert body['error'] == 'Missing password'
 
 
-# 5. и без ответа
 def test_delete_user_check_empty_response():
     response = requests.delete('https://reqres.in/api/users/2')
     print(response.text)
@@ -73,8 +66,6 @@ def test_single_user_not_found_returns_empty_body():
     body = response.json()
     assert body == {}
 
-
-# На разные схемы (4-5 схем)
 
 def test_create_user_validate_schema():
     response = requests.post('https://reqres.in/api/users', data={"name":"morpheus","job":"leader"})
@@ -104,7 +95,7 @@ def test_change_status_code():
     assert response.status_code == 200
 
 
-# 6. На бизнес-логику
+
 def test_if_username_and_job_updated():
     name = 'alla'
     job = 'doctor'
